8c543b5c633dccf0db6221f5099860acc89e3de4
Replit user <>
2023-09-07 21:54:28 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -115,7 +115,7 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair *searchMap(HashMap * map,  char * key) {
-  int i=hash(key,map->pairs);
+  int i=hash(key) % map->size;
 
   while(1){
     Pair* current=map->pairs[i];

87957e9ae5fad675dbd408d367823e5d688178d7
Replit user <>
2023-09-08 16:12:45 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -149,14 +149,14 @@ Pair *searchMap(HashMap * map,  char * key) {
 }
 
 Pair *firstMap(HashMap * map) {
-  for (long i = 0; i < map->capacity; i++) {
-      if (map->buckets[i] != NULL) {
-          map->current = i;
-          return map->buckets[i];
-      }
-  }
   map->current = -1; 
-  return NULL;
+  Pair *firstPair = nextMap(map);
+  if (firstPair != NULL) {
+      return firstPair->value;
+  } 
+  else {
+      return -1;
+  }
 }
 
 Pair *nextMap(HashMap * map) {
